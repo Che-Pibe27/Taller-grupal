3dd71f46f028093e26b540521e6ab5fd569f2b4b
Pablo Ribeiro <pabloribeiro2302@gmail.com>
2023-10-05 10:21:03 -0300

    commit

--- 14.2_JapFlix/js/script.js
+++ 14.2_JapFlix/js/script.js
@@ -2,65 +2,62 @@ const API_URL = 'https://japceibal.github.io/japflix_api/movies-data.json';
 const container = document.getElementById('lista');
 const btnBuscar  = document.getElementById('btnBuscar');
 const inputBuscar = document.getElementById('inputBuscar')
+let movieArray = [];
 
 fetch(API_URL)
 .then(response => response.json())
-.then(data => createCards(data))
+.then(data => movieArray = data)
+.then(createCards(movieArray))
 .catch("error");
 
-
-
-
-function createCards(obj){
-    container.innerHTML += `
+function createCards(array){
+    container.innerHTML = ''
+    for(let item of array){
+        container.innerHTML += `
 <div>
     <div class="cardText">
-        <h2 class="title">${obj.title}</h2>
-        <p class="tagline">${obj.tagline}</p>
+        <h2 class="title text-light">${item.title}</h2>
+        <p class="tagline text-light">${item.tagline}</p>
     </div>
-    <div class="score">
-        <p class="score">${obj.vote_average}</p>
+    <div class="score" id="${item.id}">
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span> 
+          <span class="fa fa-star"></span> 
+          <span class="fa fa-star"></span> 
+          <span class="fa fa-star"></span>
     </div>    
-</div>`;
-}
+</div>`
 
+const starContainer = document.getElementById(`${item.id}`);
+let estrellas = starContainer.querySelectorAll(".fa.fa-star");
+console.log(estrellas)
+estrellas.forEach((star, index) => {
+   let voteAverage = item.vote_average / 2;
+   if(index < Math.round(voteAverage)){
+    star.classList.add('checked')
+   } else {
+    star.classList.add('text-light')
+   }
+})
+    }
+}
 
 
 
  btnBuscar.addEventListener('click', ()=> {
     if (inputBuscar.value !== '') {
-        let filterData = [];
         const searchTerm = inputBuscar.value.toLowerCase();
-        const filteredProducts = filterData.filter((movies) =>
+        
+        const filteredProducts = movieArray.filter((movies) =>
         (movies?.title?.toLowerCase().includes(searchTerm) || 
         movies?.tagline?.toLowerCase().includes(searchTerm) || 
         movies?.overview?.toLowerCase().includes(searchTerm) ||
-        movies?.generes?.toLowerCase().includes(searchTerm))
-        );
+        (movies.genres.some((genre) => genre.name.toLowerCase().includes(searchTerm)))));
+
         createCards(filteredProducts); 
+    }
+    else {
+        alert('Ingrese término de busqueda')
     }
 }
 )
-
-
-
-
-
-/*async function fetchData() {
-    await fetch(PRODUCT_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch("error");
-  }
-  console.log(fetchData());*/
-
-  /*function initialize() {
-    searchInput.addEventListener("input", () => {
-      const searchTerm = searchInput.value.toLowerCase();
-      const filteredProducts = filterData.filter((product) => 
-      (product?.name?.toLowerCase().includes(searchTerm) || product?.description?.toLowerCase().includes(searchTerm))
-      );
-      createProducts(filteredProducts);
-    });*/
